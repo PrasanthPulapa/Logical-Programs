24e1a96e46608a562b97a775141649f503bb571b
HarishArigela <harishbabaarigela@gmail.com>
2025-09-30 19:20:30 +0530

    ArmstrongCode commit

---
+++ ArmstrongNumCheck.java
@@ -0,0 +1,32 @@
+//Armstrong number code
+
+package com;
+
+import java.util.Scanner;
+
+public class AmstrongNumCheck {
+	public static void main(String[] args) {
+		System.out.println("Enter a number:");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		AmstrongNumCheck a = new AmstrongNumCheck();
+		int result = a.armstrongCheck(num);
+		if (num == result) {
+			System.out.println(num + " is a armstrong number");
+		}
+		else {
+			System.out.println(num + " is not a armstrong number");
+		}
+		scan.close();
+	}
+	
+	int armstrongCheck(int num) {
+		int result = 0;
+		while(num != 0) {
+			int digit = num % 10;
+			result = result + (digit*digit*digit);
+			num = num/10;
+		}
+		return result;
+	}
+}
