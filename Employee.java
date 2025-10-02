7540a5be771d02591430144b09f36d4b1425bae6
PrasanthPulapa^c <pulapaprasanth@gmail.com>
2025-10-02 16:46:55 +0530

    New Line Added

--- Employee.java
+++ Employee.java
@@ -1,5 +1,10 @@
 public class Employee {
+
+    public static int x = 10;
+    public static int y = 20;
     public static void main(String[] args) {
         System.out.println("Hello Baaa");
+        System.out.println(x + y);
     }
+
 }
